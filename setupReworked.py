3f300decd552ef6e2111ba03082467c3dfbff5c6
Akbar Pathan <akbar.pathan034@gmail.com>
2022-12-13 18:55:26 -0500

    basically finished project (dirty)

--- setupReworked.py
+++ setupReworked.py
@@ -7,17 +7,9 @@ startDate = '2017-12-01'
 endDate = '2022-12-01'
 
 def chartPrice(ticker, beginning, end):
+
     # Grabbing data from Yahoo Finance using Pandas Datareader
     adjClose = web.DataReader(ticker, 'yahoo', beginning, end)['Adj Close']
-
-    # Isolating dates and prices into their own separate lists
-    dateList = []
-    for i in range (len(adjClose.index)):
-        dateList.append(str(adjClose.index[i].date()))
-
-    priceList = []
-    for i in range (len(adjClose.index)):
-        priceList.append(adjClose[i])
 
     ma200 = ta.sma(adjClose, 200, talib=True)
     ma50 = ta.sma(adjClose, 50, talib=True)
@@ -27,23 +19,57 @@ def chartPrice(ticker, beginning, end):
     plt.plot(ma200)
     plt.plot(ma50)
 
-    ma200Updated = ma200.fillna(0)
-    ma50Updated = ma50.fillna(0)
-
-    ma200Values = []
-    for i in range (len(ma200Updated.index)):
-        ma200Values.append(ma200Updated[i])
-
-    ma50Values = []
-    for i in range (len(ma50Updated.index)):
-        ma50Values.append(ma50Updated[i])
-    
-
     intersections = np.argwhere(np.diff(np.sign(ma50 - ma200))).flatten()
-    plt.scatter(adjClose.index[intersections], ma50[intersections], color='red')
-    print(type(intersections))
-    print(intersections)
+    newIntersections = intersections[199:]
+    plt.scatter(adjClose.index[newIntersections], ma50[newIntersections], color='purple')
 
+    calculateROI(adjClose, ma200, ma50, newIntersections)
+
+    plotChart(ticker, beginning, end)
+
+
+stockReturns = []
+
+def calculateROI(price, ma200, ma50, crosses):
+    ma50Slope = 0
+    ma200Slope = 0
+    buySignalIndex = []
+    sellSignalIndex = []
+    order = []
+    ROI = 0.0
+    for i in crosses:
+        ma50Slope = (ma50[i+1] - ma50[i-1])/2
+        ma200Slope = (ma200[i+1] - ma200[i-1])/2
+        if (ma50Slope > ma200Slope):
+            order.append("Buy")
+            buySignalIndex.append(i)
+            # Make the intersection green as well
+        else:
+            order.append("Sell")
+            sellSignalIndex.append(i)
+            # Make the intersection red as well
+
+    if (order[0]=="Sell"):
+        del order[0]
+        del sellSignalIndex[0]
+    if (order[len(order)-1]=="Buy"):
+        del order[len(order)-1]
+        del buySignalIndex[len(buySignalIndex)-1]
+
+    for i in range(len(buySignalIndex)):
+        ROI += (price[sellSignalIndex[i]]-price[buySignalIndex[i]])/price[buySignalIndex[i]]
+    
+    stockReturns.append(ROI)
+
+def calculateTotalROI():
+    avgReturn = 0.0
+    for i in range (len(stockReturns)):
+        avgReturn += stockReturns[i]
+    avgReturn = avgReturn/len(stockReturns)
+    avgReturn = str(round(avgReturn*100)) + '%'
+    print("The returns of this portfolio with this trading strategy is " + avgReturn)
+
+def plotChart(ticker, beginning, end):
     # Making additions to chart for presentation
     plt.ylabel('Adjusted Close Price (in $)')
     plt.xlabel('Date')
@@ -52,7 +78,15 @@ def chartPrice(ticker, beginning, end):
     plt.legend(["Adj Close", "200D MA", "50D MA"], loc="upper left")
     plt.show()
 
-    
-    
 chartPrice("PFE", startDate, endDate)
-#chartPrice("AAPL", startDate, endDate)
+chartPrice("AAPL", startDate, endDate)
+chartPrice("NKE", startDate, endDate)
+chartPrice("F", startDate, endDate)
+chartPrice("JPM", startDate, endDate)
+chartPrice("CHGG", startDate, endDate)
+chartPrice("ETD", startDate, endDate)
+chartPrice("CROX", startDate, endDate)
+chartPrice("FIVE", startDate, endDate)
+chartPrice("FHN", startDate, endDate)
+
+calculateTotalROI()
